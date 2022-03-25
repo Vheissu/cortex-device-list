463144ddbb3c6c9ad45bd813c3652ed2f9ca5f66
Dwayne <dwaynecharrington@gmail.com>
2022-03-25 14:18:27 +1000

    feat(site): added in cabs list

--- src/my-app.ts
+++ src/my-app.ts
@@ -1,11 +1,19 @@
 import amps from "./data/amps.json";
+import cabs from "./data/cabs.json";
 
 export class MyApp {
+  private currentTab = 'amps';
+
   private amps = amps;
+  private cabs = cabs;
 
   private filters = [
     { value: '', keys: ['name'] },
     { value: '', keys: ['real'] },
     { value: '', keys: ['type'] }
   ];
+
+  toggleTab(tab) {
+    this.currentTab = tab;
+  }
 }

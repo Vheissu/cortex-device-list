1f78b12eeae7d213f46d07c45b4d9be2c63ceccc
Dwayne Charrington <dwaynecharrington@gmail.com>
2022-03-26 20:49:00 +1000

    feat(site): break up table views into separate components

--- src/my-app.ts
+++ src/my-app.ts
@@ -1,31 +1,39 @@
-import amps from "./data/amps.json";
-import cabs from "./data/cabs.json";
-import effects from "./data/effects.json";
+import amps from './data/amps.json';
+import cabs from './data/cabs.json';
+import effects from './data/effects.json';
+
+import AmpsTable from './amps-table.html';
+import CabsTable from './cabs-table.html';
+import EffectsTable from './effects-table.html';
 
 export class MyApp {
-  private currentTab = 'amps';
+    private currentTab = 'amps';
 
-  private amps = amps;
-  private cabs = cabs;
-  private effects = effects;
+    private amps = amps;
+    private cabs = cabs;
+    private effects = effects;
 
-  private filters = [
-    { value: '', keys: ['name'] },
-    { value: '', keys: ['real'] },
-    { value: '', keys: ['type'] },
-    { value: '', keys: ['irAuthor'] },
-    { value: '', keys: ['deviceType'] },
-  ];
+    private ampsView = AmpsTable;
+    private cabsView = CabsTable;
+    private effectsView = EffectsTable;
 
-  getCab(cabId) {
-    return this.cabs.find(c => c.id === cabId) ?? null;
-  }
+    private filters = [
+        { value: '', keys: ['name'] },
+        { value: '', keys: ['real'] },
+        { value: '', keys: ['type'] },
+        { value: '', keys: ['irAuthor'] },
+        { value: '', keys: ['deviceType'] },
+    ];
 
-  toggleTab(tab) {
-    this.currentTab = tab;
-
-    for (const filter of this.filters) {
-      filter.value = '';
+    getCab(cabId) {
+        return this.cabs.find((c) => c.id === cabId) ?? null;
     }
-  }
+
+    toggleTab(tab) {
+        this.currentTab = tab;
+
+        for (const filter of this.filters) {
+            filter.value = '';
+        }
+    }
 }
